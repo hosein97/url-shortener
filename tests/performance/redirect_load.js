73dc14772fe98fddaeb8ef5887b7a9361e2c3583
hosein <hosein.panahi98@gmail.com>
2026-06-02 09:04:55 -0400

    Add k6 to test redirect load

---
+++ tests/performance/redirect_load.js
@@ -0,0 +1,18 @@
+import http from "k6/http";
+import { check } from "k6";
+
+export const options = {
+    vus: 10,
+    duration: "30s",
+};
+
+export default function () {
+    const response = http.get(
+        "http://web:8000/GEcZAa/", {redirects: 0}
+    );
+
+    check(response, {
+        "status is redirect": (r) =>
+            r.status === 301 || r.status === 302,
+    });
+}
